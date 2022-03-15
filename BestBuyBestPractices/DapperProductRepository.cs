22ef5c63edca71eb434e74d0f7cec0c07398ef07
Maddi Wade <Madelinenwade@gmail.com>
2022-03-15 11:13:43 -0400

    done with 2

---
+++ BestBuyBestPractices/DapperProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+
+namespace BestBuyBestPractices
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+        public DapperProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        
+       public void CreateProduct(string newname, double newprice, int newcategoryID)
+        {
+            _connection.Execute("INSERT INTO products (name, price, categoryID) VALUES (@name, @price, @categoryID);",
+                new { name = newname, price = newprice, categoryID = newcategoryID });
+        }
+
+       public IEnumerable<Product> GetAllProducts()
+        {
+            return _connection.Query<Product>("SELECT * FROM PRODUCTS");
+        }
+
+    }
+}
--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+
+
 
 namespace BestBuyBestPractices
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var repo = new DapperProductRepository(conn);
+
+            repo.CreateProduct("DVD", 12.99, 10);
+
+            var products = repo.GetAllProducts();
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.CategoryID);
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.StockLevel);
+                Console.WriteLine(product.OnSale);
+            }
+
+            
+            
         }
-    }
-}
+    } 
+ }
+
